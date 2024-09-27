2c071c32820748eec09e1c31ae743cfd2e72710d
Vinicius Almeida <vinicius.vinibola@gmail.com>
2024-09-27 10:36:31 -0300

    teste

--- TrabalhoPOO/Program.cs
+++ TrabalhoPOO/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello World!");
+
+int teste = 10;
+Console.WriteLine(teste);
--- TrabalhoPOO/obj/Debug/net8.0/TrabalhoPOO.AssemblyInfo.cs
+++ TrabalhoPOO/obj/Debug/net8.0/TrabalhoPOO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrabalhoPOO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7962a41b6000fb9ab696c1e80c444c67b17f82d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrabalhoPOO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrabalhoPOO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
