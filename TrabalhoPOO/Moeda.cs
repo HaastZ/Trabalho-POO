bccefdab49d83dab5aa55c146361cd8b24b9d760
Lucas Bryan S. Quintas <102496302+BryanQuintas@users.noreply.github.com>
2024-10-02 20:30:16 -0300

    Atualizando a classe Moeda para que realize a conversão de real para dolar
    novo método: ConverterMoeda
    usando como base: 1 Dolar = 5,45 Real

--- TrabalhoPOO/Moeda.cs
+++ TrabalhoPOO/Moeda.cs
@@ -1,20 +1,58 @@
 public class Moeda
 {
-    private string BRL;
-    private string USD;
-    public Moeda(string BRL, string USD) 
+    private string tipoMoeda;
+    private double valor;
+
+    // Iniciador
+    public Moeda(string tipoMoeda, double valor)
     {
-        this.BRL = BRL;
-        this.USD = USD;
+        this.tipoMoeda = tipoMoeda;
+        this.valor = valor;
     }
 
-    public string getBRL() 
+    // Método que "pega" a informação de qual tipo de moeda será usada no Voo
+    public string GetTipoMoeda()
     {
-        return this.BRL;
+        return this.tipoMoeda;
     }
 
-    public string getUSD() 
+    // Método que "pega" a informação do valor do voo
+    public double GetValor()
     {
-        return this.USD;
+        return this.valor;
+    }
+
+    // Método de conversão de USD para BRL (ou vice-versa)
+    public string ConverterMoeda(string moedaVoo)
+    {
+        double valorConvertido = 0; //Variável que armazena o valor do voo após converção
+        double taxaConversaoBRLParaUSD = 5.45; // Considerando a taxa de conversão de 1 USD = 5,45 BRL
+
+        if (tipoMoeda == "BRL")
+        {
+            valorConvertido = valor; // Se as moedas forem iguais, o valor é o mesmo
+        }
+        else if (tipoMoeda == "USD")
+        {
+            valorConvertido = valor/taxaConversaoBRLParaUSD; // Converter de real para dólar
+        }
+        else
+        {
+            throw new Exception("Conversão de moeda não suportada.\r\nNosso sistema ultiliza o real como base e apenas converte para Dolar em casos de Voos Internacionais! :)");
+        }
+
+        // Verifica moedaVoo e formata o valor como nos Exemplos: R$500,00 e US$100,00
+        if (moedaVoo == "BRL")
+        {
+            return "R$" + valorConvertido.ToString("N2");
+        }
+        else if (moedaVoo == "USD")
+        {
+            return "US$" + valorConvertido.ToString("N2");
+        }
+        else
+        {
+            throw new Exception("Conversão de moeda não suportada.\r\nNosso sistema ultiliza o real como base e apenas converte para Dolar em casos de Voos Internacionais! :)");
+        }
     }
 }
