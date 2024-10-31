7e84ee78dedc46f4d252d40abdc491ad7f404040
Lucas Bryan S. Quintas <102496302+BryanQuintas@users.noreply.github.com>
2024-10-31 15:12:05 -0300

    Create Bilhete.cs

---
+++ TrabalhoPOO/Bilhete.cs
@@ -0,0 +1,33 @@
+namespace TrabalhoPOO
+{
+    public class Bilhete
+    {
+        private Passagem passagem;
+
+        public Bilhete(Passagem passagem)
+        {
+            this.passagem = passagem;
+        }
+
+        public void CriarBilhete()
+        {
+            var passageiro = passagem.GetPassageiro();
+            var nome = passageiro.getNome();
+            var cpf = passageiro.getNumeroDocumento();
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine( $"Nome: {nome}          CPF: {cpf}");
+
+            foreach (var vooProgramado in passagem.GetVooProgramado())
+            {
+                var origem = vooProgramado.GetOrigem();
+                var destino = vooProgramado.GetDestino(); 
+
+                Console.WriteLine();
+                Console.WriteLine($"Origem: {origem}   Destino: {destino}");
+            }
+
+            Console.WriteLine("-------------------------------------");
+        }
+    }
+}
