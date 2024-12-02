e4dacceedd5ae627e6414bd447fc041d3ce55236
Vinicius Almeida <vinicius.vinibola@gmail.com>
2024-12-02 17:08:56 -0300

    git merge - Branch Artur

--- TrabalhoPOO/obj/Debug/net8.0/TrabalhoPOO.AssemblyInfo.cs
+++ TrabalhoPOO/obj/Debug/net8.0/TrabalhoPOO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrabalhoPOO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41791538a8f00f5f2c097fbf174b291db4fb7776")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c37c4e78a546f88395e62d30d83753901720cfb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrabalhoPOO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrabalhoPOO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
