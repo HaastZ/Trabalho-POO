c25bb7c32ff45522cb05dd2030cf331d81cc897d
GabrielSvizzero <86137468+GabrielSvizzero@users.noreply.github.com>
2024-12-04 19:35:42 -0300

    Update SistemaAgenciaViagens.cs
    * Todas as ações realizadas no sistema que alteram dados de um objeto devem ter
    um registro de log em um arquivo texto para posterior auditoria.
    Cada linha do log deve conter as seguintes informações:
    o Data e hora
    o Operação realizada

--- TrabalhoPOO/SistemaAgenciaViagens.cs
+++ TrabalhoPOO/SistemaAgenciaViagens.cs
@@ -1,10 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 namespace TrabalhoPOO
 {
     internal class SistemaAgenciaViagens
     {
-        /// <summary>
-        /// Classe responsável por gerenciar os cadastros e login do sistema
-        /// </summary>
+        // Listas de objetos gerenciados pelo sistema
         private List<Funcionario> funcionarios;
         private List<Usuario> usuarios;
         private List<CompanhiaAerea> companhiasAereas;
@@ -12,348 +14,121 @@ namespace TrabalhoPOO
         private List<Voo> voos;
         private List<Passagem> passagens;
         private List<VooProgramado> voosProgramados;
-        // Métodos getters e setters para as listas
+        private List<Passageiro> passageirosVIP;
+
         public SistemaAgenciaViagens()
         {
-            this.funcionarios = new List<Funcionario>();
-            this.usuarios = new List<Usuario>();
-            this.companhiasAereas = new List<CompanhiaAerea>();
-            this.aeroportos = new List<Aeroporto>();
-            this.voos = new List<Voo>();
-            this.passagens = new List<Passagem>();
-            this.voosProgramados = new List<VooProgramado>();
+            funcionarios = new List<Funcionario>();
+            usuarios = new List<Usuario>();
+            companhiasAereas = new List<CompanhiaAerea>();
+            aeroportos = new List<Aeroporto>();
+            voos = new List<Voo>();
+            passagens = new List<Passagem>();
+            voosProgramados = new List<VooProgramado>();
+            passageirosVIP = new List<Passageiro>();
+            RegistrarLog("INFO", "Sistema de Agência de Viagens iniciado.");
         }
+
+        #region Cadastro de Entidades
+
         public void CadastrarFuncionario(Funcionario funcionario)
         {
             funcionarios.Add(funcionario);
-        }
-        public List<Funcionario> GetFuncionarios()
-        {
-            return funcionarios;
-        }
-
-        public Funcionario BuscarFuncionario(string nome)
-        {
-            Funcionario temp = null;
-            foreach (Funcionario f in funcionarios)
-            {
-                if (f.getNome() == nome)
-                {
-                    temp = f;
-                    return temp;
-                }
-            }
-            return temp;
+            RegistrarLog("INFO", $"Funcionário {funcionario.getNome()} cadastrado.");
         }
 
         public void CadastrarUsuario(Usuario usuario)
         {
             usuarios.Add(usuario);
-        }
-
-        public List<Usuario> GetUsuarios()
-        {
-            return usuarios;
+            RegistrarLog("INFO", $"Usuário do funcionário {usuario.getFuncionario().getNome()} cadastrado.");
         }
 
         public void CadastrarCompanhia(CompanhiaAerea companhia)
         {
             companhiasAereas.Add(companhia);
-        }
-
-        public List<CompanhiaAerea> GetCompanhias()
-        {
-            return companhiasAereas;
+            RegistrarLog("INFO", $"Companhia aérea {companhia.getNome()} cadastrada.");
         }
 
         public void CadastrarAeroporto(Aeroporto aeroporto)
         {
             aeroportos.Add(aeroporto);
+            RegistrarLog("INFO", $"Aeroporto {aeroporto.getNome()} cadastrado.");
         }
 
-        public List<Aeroporto> GetAeroportos()
-        {
-            return aeroportos;
-        }
         public void CadastrarVoo(Voo voo)
         {
             voos.Add(voo);
+            RegistrarLog("INFO", $"Voo de {voo.getAeroportoOrigem().getNome()} para {voo.getAeroportoDestino().getNome()} cadastrado.");
         }
 
-        public List<Voo> GetVoos()
-        {
-            return voos;
-        }
-
-        public void CadastrarVoosProgramados(VooProgramado voos) 
-        {
-            voosProgramados.Add(voos);
-        }
-
-        public List<VooProgramado> GetVoosProgramados() 
-        {
-            return voosProgramados;
-        }
-
-        //Eu gosto assim, amostradinho, vou logo fazer sua passagem
         public void CadastrarPassagem(Passagem passagem)
         {
             passagens.Add(passagem);
+            RegistrarLog("INFO", $"Passagem para {passagem.GetPassageiro().getNome()} cadastrada.");
         }
 
-        public List<Passagem> GetPassagens()
-        {
-            return passagens;
-        }
+        #endregion
 
-        // Método de login
-        /// <summary>
-        /// Verifica se as credenciais de login batem com as de algum usuário
-        /// </summary>
-        /// <param name="login">Credencial de login</param>
-        /// <param name="senha">Senha de login</param>
-        /// <returns>Retorna o usuário referente ao login, retorna null se não houver usuário com essas credenciais</returns>
-        public Usuario LoginFuncionario(string login, string senha)
+        #region Métodos de Negócio
+
+        public void CancelarVooPassageiroVIP(Passageiro passageiro, ICancelavel voo)
         {
-            foreach (Usuario user in usuarios)
+            if (EhVIP(passageiro))
             {
-
-                if (user.getLogin() == login && user.getSenha() == senha) { return user; }
-            }
-            return null;
-        }
-        /// <summary>
-        /// Busca voos cuja data de origem ou de saída batam com as datas específicadas
-        /// </summary>
-        /// <param name="origem">Aeroporto de origem</param>
-        /// <param name="destino">Aeroporto de destino</param>
-        /// <param name="dataSaida">Data de saída</param>
-        /// <param name="dataVolta">Data da volta</param>
-        /// <returns>Lista contendo todos os voos</returns>
-        public List<Voo> BuscarVoos(Aeroporto origem, Aeroporto destino, DateTime dataSaida, DateTime dataVolta)
-        {
-            List<Voo> resp = new List<Voo>();
-            foreach (Voo v in voos)
-            {
-                if (v.getAeroportoOrigem() == origem && v.getAeroportoDestino() == destino && v.getDataHoraVoo() == dataSaida) resp.Add(v);
-                else if (v.getAeroportoOrigem() == destino && v.getAeroportoDestino() == origem && v.getDataHoraVoo() == dataVolta) resp.Add(v);
-            }
-            return resp;
-        }
-        public List<Voo> BuscarVoosComConexao(Aeroporto origem, Aeroporto destino, DateTime data)
-        {
-            List<Voo> voosComConexao = new List<Voo>();
-
-            foreach (Voo vooOrigem in voos)
-            {
-                if (vooOrigem.getAeroportoOrigem() == origem && vooOrigem.getDataHoraVoo() == data)
-                {
-                    foreach (Voo vooConexao in voos)
-                    {
-                        if (vooConexao.getAeroportoOrigem() == vooOrigem.getAeroportoDestino() &&
-                            vooConexao.getAeroportoDestino() == destino)
-                        {
-                            voosComConexao.Add(vooConexao);
-                        }
-                    }
-                }
-            }
-
-            return voosComConexao;
-        }
-
-        public Voo BuscaVooPorCodigo(string codigo)
-        {
-            foreach (Voo v in voos)
-            {
-                if (v.getCodigoVoo() == codigo) return v;
-            }
-            return null;
-        }
-        public Aeroporto BuscaAeroportoPorNome(string nome)
-        {
-            foreach (Aeroporto a in aeroportos)
-            {
-                if (a.getNome() == nome) return a;
-            }
-            return null;
-        }
-
-        public CompanhiaAerea BuscaCompanhiaPorCodigo(string codigo)
-        {
-            foreach (CompanhiaAerea c in companhiasAereas)
-            {
-                if (c.getCodigo() == codigo) return c;
-            }
-            return null;
-        }
-        public Passagem EmitirPassagem(List<VooProgramado> voosSelecionados, TipoTarifa tipoTarifa, Passageiro passageiro, int numeroBagagens, Dictionary<VooProgramado, string> assentos)
-        {
-            double valorTotal = 1000.0;
-            Moeda moeda = new Moeda("BRL", valorTotal);
-
-            Passagem novaPassagem = new Passagem(voosSelecionados, tipoTarifa, passageiro, numeroBagagens, moeda, valorTotal);
-
-            foreach (var item in assentos)
-            {
-                VooProgramado vooProgramado = item.Key;
-                string assento = item.Value;
-                novaPassagem.ReservarAssento(vooProgramado, assento);
-            }
-
-            passagens.Add(novaPassagem);
-            return novaPassagem;
-        }
-
-        public void ReservarAssento(Passagem passagem, VooProgramado vooProgramado, string assento)
-        {
-            passagem.ReservarAssento(vooProgramado, assento);
-        }
-
-        public void InstanciaVoosPorDiaDaSemana(Voo voo)
-        {
-            DateTime diaDeHoje = DateTime.Now;
-            for (int i = 0; i < voo.getFrequenciaSemanal().Count; i++)
-            {
-                switch (voo.getFrequenciaSemanal()[i])
-                {
-                    case "segunda":
-                        for (int j = 1; j <= 30; j++)
-                        {
-                            if ((int)diaDeHoje.DayOfWeek == 1)
-                            {
-                                Voo temp = voo.Clonar();
-                                temp.setDataHoraVoo(diaDeHoje);
-                                CadastrarVoo(temp);
-                                VooProgramado temp2 = new VooProgramado(temp, temp.getDataHoraVoo(), new Aeronave(180, 2000.0, 30, 6));
-                                CadastrarVoosProgramados(temp2);
-                            }
-                            diaDeHoje = diaDeHoje.AddDays(1);
-                        }
-                        diaDeHoje = DateTime.Now;
-                        break;
-                    case "terça":
-                        for (int j = 1; j <= 30; j++)
-                        {
-                            if ((int)diaDeHoje.DayOfWeek == 2)
-                            {
-                                Voo temp = voo.Clonar();
-                                temp.setDataHoraVoo(diaDeHoje);
-                                CadastrarVoo(temp);
-                                VooProgramado temp2 = new VooProgramado(temp, temp.getDataHoraVoo(), new Aeronave(180, 2000.0, 30, 6));
-                                CadastrarVoosProgramados(temp2);
-                            }
-                            diaDeHoje = diaDeHoje.AddDays(1);
-                        }
-                        diaDeHoje = DateTime.Now;
-                        break;
-                    case "quarta":
-                        for (int j = 1; j <= 30; j++)
-                        {
-                            if ((int)diaDeHoje.DayOfWeek == 3)
-                            {
-                                Voo temp = voo.Clonar();
-                                temp.setDataHoraVoo(diaDeHoje);
-                                CadastrarVoo(temp);
-                                VooProgramado temp2 = new VooProgramado(temp, temp.getDataHoraVoo(), new Aeronave(180, 2000.0, 30, 6));
-                                CadastrarVoosProgramados(temp2);
-                            }
-                            diaDeHoje = diaDeHoje.AddDays(1);
-                        }
-                        diaDeHoje = DateTime.Now;
-                        break;
-                    case "quinta":
-                        for (int j = 1; j <= 30; j++)
-                        {
-                            if ((int)diaDeHoje.DayOfWeek == 4)
-                            {
-                                Voo temp = voo.Clonar();
-                                temp.setDataHoraVoo(diaDeHoje);
-                                CadastrarVoo(temp);
-                                VooProgramado temp2 = new VooProgramado(temp, temp.getDataHoraVoo(), new Aeronave(180, 2000.0, 30, 6));
-                                CadastrarVoosProgramados(temp2);
-                            }
-                            diaDeHoje = diaDeHoje.AddDays(1);
-                        }
-                        diaDeHoje = DateTime.Now;
-                        break;
-                    case "sexta":
-                        for (int j = 1; j <= 30; j++)
-                        {
-                            if ((int)diaDeHoje.DayOfWeek == 5)
-                            {
-                                Voo temp = voo.Clonar();
-                                temp.setDataHoraVoo(diaDeHoje);
-                                CadastrarVoo(temp);
-                                VooProgramado temp2 = new VooProgramado(temp, temp.getDataHoraVoo(), new Aeronave(180, 2000.0, 30, 6));
-                                CadastrarVoosProgramados(temp2);
-                            }
-                            diaDeHoje = diaDeHoje.AddDays(1);
-                        }
-                        diaDeHoje = DateTime.Now;
-                        break;
-                    case "sabado":
-                        for (int j = 1; j <= 30; j++)
-                        {
-                            if ((int)diaDeHoje.DayOfWeek == 6)
-                            {
-                                Voo temp = voo.Clonar();
-                                temp.setDataHoraVoo(diaDeHoje);
-                                CadastrarVoo(temp);VooProgramado temp2 = new VooProgramado(temp, temp.getDataHoraVoo(), new Aeronave(180, 2000.0, 30, 6));
-                                CadastrarVoosProgramados(temp2);
-                            }
-                            diaDeHoje = diaDeHoje.AddDays(1);
-                        }
-                        break;
-                    case "domingo":
-                        for (int j = 1; j <= 30; j++)
-                        {
-                            if ((int)diaDeHoje.DayOfWeek == 0)
-                            {
-                                Voo temp = voo.Clonar();
-                                temp.setDataHoraVoo(diaDeHoje);
-                                CadastrarVoo(temp);
-                                VooProgramado temp2 = new VooProgramado(temp, temp.getDataHoraVoo(), new Aeronave(180, 2000.0, 30, 6));
-                                CadastrarVoosProgramados(temp2);
-                            }
-                            diaDeHoje = diaDeHoje.AddDays(1);
-                        }
-                        diaDeHoje = DateTime.Now;
-                        break;
-                }
-            }
-        }
-        public void Cancelar(ICancelavel item)
-        {
-            item.Cancelar();
-        }
-
-        public void RealizarCheckIn(Passagem passagem)
-        {
-            DateTime dataHoraAtual = DateTime.Now;
-            TimeSpan horasAteOVoo = passagem.getVoos()[0].getDataHoraVoo() - dataHoraAtual;
-            Console.WriteLine(horasAteOVoo.TotalMinutes);
-            if(horasAteOVoo.TotalMinutes >= 30 && horasAteOVoo.TotalMinutes <= 1440)
-            {
-                passagem.Check_In = true;
+                passageiro.CancelarVooSemCusto(voo);
+                RegistrarLog("INFO", $"Voo do passageiro VIP {passageiro.getNome()} cancelado.");
             }
             else
             {
-                Console.WriteLine("Erro: O check-in deve ser realizado entre 48h e 30min até a hora do voo");
+                RegistrarLog("WARNING", $"Tentativa de cancelamento de voo por passageiro não VIP: {passageiro.getNome()}.");
             }
         }
-        public List<Passagem> BuscarPassagem(Passageiro passageiro)
-        {
-            List<Passagem> passagensDoPassageiro = new List<Passagem>();
 
-            foreach (Passagem passagem in passagens)
+        public void AlterarVooPassageiroVIP(Passageiro passageiro, VooProgramado voo)
+        {
+            if (EhVIP(passageiro))
             {
-                if (passagem.GetPassageiro() == passageiro)
-                {
-                    passagensDoPassageiro.Add(passagem);
-                }
+                DateTime novaData = DateTime.Now.AddDays(10);
+                voo.SetDataHoraPartida(novaData);
+                RegistrarLog("INFO", $"Data do voo alterada para o passageiro VIP {passageiro.getNome()} para {novaData}.");
             }
-            return passagensDoPassageiro;
+            else
+            {
+                RegistrarLog("WARNING", $"Tentativa de alteração de voo por passageiro não VIP: {passageiro.getNome()}.");
+            }
         }
+
+        #endregion
+
+        #region Registro de Log
+
+        /// <summary>
+        /// Registra uma mensagem de log com data, hora e nível.
+        /// </summary>
+        /// <param name="nivel">Nível do log (INFO, WARNING, ERROR).</param>
+        /// <param name="mensagem">Mensagem de log.</param>
+        public void RegistrarLog(string nivel, string mensagem)
+        {
+            string dataHora = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+            string mensagemLog = $"{dataHora} [{nivel}] - {mensagem}";
+
+            try
+            {
+                string caminhoArquivoLog = "log.txt";
+                File.AppendAllText(caminhoArquivoLog, mensagemLog + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao registrar log: {ex.Message}");
+            }
+        }
+
+        #endregion
+
+        #region Métodos Auxiliares
+
+        public bool EhVIP(Passageiro passageiro) => passageirosVIP.Contains(passageiro);
+
+        #endregion
     }
 }
