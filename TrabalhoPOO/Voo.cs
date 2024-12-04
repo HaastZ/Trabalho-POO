be00481eeaa25d265f731104f5e540c44b937841
GabrielSvizzero <86137468+GabrielSvizzero@users.noreply.github.com>
2024-12-04 19:09:54 -0300

    Update Voo.cs
    * O horário previsto de chegada deve ser calculado com base no horário de par-
    tida do vôo e no tempo de viagem calculado no item anterior.

--- TrabalhoPOO/Voo.cs
+++ TrabalhoPOO/Voo.cs
@@ -1,15 +1,20 @@
-public class Voo
+public class Voo : ILog
 {
     private Aeroporto aeroportoOrigem;
     private Aeroporto aeroportoDestino;
     private DateTime dataHoraVoo;
     private string codigoVoo;
     private List<string> frequenciaSemanal;
-    private string horaPartida;
     private string duracao;
     private CompanhiaAerea companhiaAerea;
-    private TipoTarifa tipoTarifa; private Moeda moeda;
-    public Voo(Aeroporto origem, Aeroporto destino, DateTime dataHoraVoo, string codigoVoo, CompanhiaAerea companhiaAerea, TipoTarifa tipoTarifa, Moeda moeda)
+    private TipoTarifa tipoTarifa;
+    private Moeda moeda;
+    
+    public TimeSpan TempoViagem { get; set; }
+    public double VelocidadeMedia { get; set; }
+    public DateTime HorarioPrevistoChegada { get; set; }
+
+    public Voo(Aeroporto origem, Aeroporto destino, DateTime dataHoraVoo, string codigoVoo, CompanhiaAerea companhiaAerea, TipoTarifa tipoTarifa, Moeda moeda, double velocidadeMedia)
     {
         this.aeroportoOrigem = origem;
         this.aeroportoDestino = destino;
@@ -18,8 +23,13 @@ public class Voo
         this.companhiaAerea = companhiaAerea;
         this.tipoTarifa = tipoTarifa;
         this.moeda = moeda;
+        VelocidadeMedia = velocidadeMedia;
+        TempoViagem = CalcularTempoViagem();
+        HorarioPrevistoChegada = CalcularHorarioPrevistoChegada();
+        RegistrarLog($"Criação do voo {this.codigoVoo}");
     }
-    public Voo(Aeroporto origem, Aeroporto destino, DateTime dataHoraVoo, string codigoVoo, CompanhiaAerea companhiaAerea, TipoTarifa tipoTarifa, Moeda moeda, List<string> frequenciaSemanal, string horaPartida, string duracao)
+
+    public Voo(Aeroporto origem, Aeroporto destino, DateTime dataHoraVoo, string codigoVoo, CompanhiaAerea companhiaAerea, TipoTarifa tipoTarifa, Moeda moeda, List<string> frequenciaSemanal, string horaPartida, string duracao, double velocidadeMedia)
     {
         this.aeroportoOrigem = origem;
         this.aeroportoDestino = destino;
@@ -29,62 +39,55 @@ public class Voo
         this.tipoTarifa = tipoTarifa;
         this.moeda = moeda;
         this.frequenciaSemanal = frequenciaSemanal;
-        this.horaPartida = horaPartida;
         this.duracao = duracao;
+        VelocidadeMedia = velocidadeMedia;
+        TempoViagem = CalcularTempoViagem();
+        HorarioPrevistoChegada = CalcularHorarioPrevistoChegada();
+        RegistrarLog($"Criação do voo {this.codigoVoo}");
     }
 
-    public List<string> getFrequenciaSemanal()
+    public List<string> getFrequenciaSemanal() => this.frequenciaSemanal;
+    public string getDuracao() => this.duracao;
+    public Aeroporto getAeroportoOrigem() => this.aeroportoOrigem;
+    public Aeroporto getAeroportoDestino() => this.aeroportoDestino;
+    public DateTime getDataHoraVoo() => this.dataHoraVoo;
+    public void setDataHoraVoo(DateTime dataHoraVoo) => this.dataHoraVoo = dataHoraVoo;
+    public string getCodigoVoo() => this.codigoVoo;
+    public CompanhiaAerea getCompanhiaAerea() => this.companhiaAerea;
+    public TipoTarifa GetTipoTarifa() => this.tipoTarifa;
+    public Moeda getMoeda() => this.moeda;
+    public Voo Clonar() => (Voo)MemberwiseClone();
+
+    private double CalculaDistanciaEntreAeroportos()
     {
-        return frequenciaSemanal;
-    }
-    public string getHoraPartida()
-    {
-        return horaPartida;
-    }
-    public string getDuracao()
-    {
-        return duracao;
+        return 110.57 * Math.Sqrt(Math.Pow(aeroportoDestino.Longitude - aeroportoOrigem.Longitude, 2) + Math.Pow(aeroportoDestino.Latitude - aeroportoOrigem.Latitude, 2));
     }
 
-    public Aeroporto getAeroportoOrigem()
+    public TimeSpan CalcularTempoViagem()
     {
-        return this.aeroportoOrigem;
+        double distancia = CalculaDistanciaEntreAeroportos();
+        double tempo = distancia / VelocidadeMedia;
+        int horas = (int)tempo;
+        tempo = tempo - horas;
+        int minutos = (int)(tempo * 60);
+        return new TimeSpan(horas, minutos, 0);
     }
 
-    public Aeroporto getAeroportoDestino()
+    public DateTime CalcularHorarioPrevistoChegada()
     {
-        return this.aeroportoDestino;
+        return dataHoraVoo.Add(TempoViagem);
     }
 
-    public DateTime getDataHoraVoo()
+    public void RegistrarLog(string operacao)
     {
-        return this.dataHoraVoo;
-    }
-    public void setDataHoraVoo(DateTime dataHoraVoo)
-    {
-        this.dataHoraVoo = dataHoraVoo;
-    }
-    public string getCodigoVoo()
-    {
-        return this.codigoVoo;
-    }
-
-    public CompanhiaAerea getCompanhiaAerea()
-    {
-        return this.companhiaAerea;
-    }
-
-    public TipoTarifa GetTipoTarifa()
-    {
-        return this.tipoTarifa;
-    }
-
-    public Moeda getMoeda()
-    {
-        return this.moeda;
-    }
-    public Voo Clonar()
-    {
-        return (Voo)MemberwiseClone();
+        try
+        {
+            string mensagem = $"{DateTime.Now:dd/MM/yyyy HH:mm:ss} - {operacao}";
+            File.AppendAllText(EncontrarArquivo.Localizar(), mensagem + Environment.NewLine);
+        }
+        catch (Exception e)
+        {
+            throw new Exception($"Ocorreu um erro ao registrar no log: {e.Message}");
+        }
     }
 }
